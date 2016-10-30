41725e90f4bf256fcba276a13fd591d1b04709f7
David Colson <david@peripherallabs.com>
2016-10-30 09:16:52 +0000

    File IO Inclusion
    If statements have now been introduced in the madlibs project, and the
    shopping list now saves using file io.

--- Intro1_Madlibs/Madlib.py
+++ Intro1_Madlibs/Madlib.py
@@ -8,22 +8,39 @@ I would prefer if she knew how to cook, clean, and wash my {7}."""
 # Pre calculation of One Eighth we can use later, written in shorthand
 oe = 1.0/8.0*100.0
 
-adjective1 = raw_input("Adjective: ")
-print("You are {}% complete!".format(oe*1))
-verbed = raw_input("Verb Ending in 'ed': ")
-print("You are {}% complete!".format(oe*2))
-pluralNoun1 = raw_input("Plural Noun: ")
-print("You are {}% complete!".format(oe*3))
-liquid = raw_input("Liquid: ")
-print("You are {}% complete!".format(oe*4))
-pluralNoun2 = raw_input("Plural Noun: ")
-print("You are {}% complete!".format(oe*5))
-noun = raw_input("Noun: ")
-print("You are {}% complete!".format(oe*6))
-nationality = raw_input("Nationality: ")
-print("You are {}% complete!".format(oe*7))
-pluralNoun3 = raw_input("Plural Noun: ")
-print("You are {}% complete!".format(oe*8))
+progress = input("Would you like a progress report? (y/n) ")
+
+adjective1 = input("Adjective: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*1))
+
+verbed = input("Verb Ending in 'ed': ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*2))
+
+pluralNoun1 = input("Plural Noun: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*3))
+
+liquid = input("Liquid: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*4))
+
+pluralNoun2 = input("Plural Noun: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*5))
+
+noun = input("Noun: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*6))
+
+nationality = input("Nationality: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*7))
+
+pluralNoun3 = input("Plural Noun: ")
+if progress == 'y':
+	print("You are {}% complete!".format(oe*8))
 
 print(madlib_template.format(adjective1, verbed, pluralNoun1, liquid, pluralNoun2, noun, nationality, pluralNoun3))
 
--- Intro2_ShoppingList/ShoppingList.py
+++ Intro2_ShoppingList/ShoppingList.py
@@ -3,9 +3,16 @@
 shoppinglist = []
 command = ""
 
+# Read in the file and prevent us from entering new things
+f = open("shoppinglist.txt", 'r')
+for line in f:
+	shoppinglist.append(line.rstrip()) # Removes newline character, show why we need this
+	command = "done"
+f.close()
+
 # We're going to loop until the user has entered all the items they want
 while command != "done":
-	command = raw_input("Please enter a shopping item, or 'done' if you are finished: ")
+	command = input("Please enter a shopping item, or 'done' if you are finished: ")
 	if command != "done":
 		shoppinglist.append(command)
 
@@ -13,12 +20,20 @@ while command != "done":
 while len(shoppinglist) > 0:
 	print(" ")
 	print("Your Shopping List")
+
+	# Print the current shopping list and save it out to a file
+	f = open("shoppinglist.txt", 'w')
 	for id, item in enumerate(shoppinglist):
+		f.write("{}\n".format(item))
 		print("{0} - {1}".format(id+1, item))
+	f.close()
 
 	# Effectively purchase an item
-	command = raw_input("Enter an item code to remove it: ")
-	del shoppinglist[int(command)-1]
+	command = input("Enter an item code to remove it (done to finish): ")
+	if command == "done":
+		quit()
+	else:
+		del shoppinglist[int(command)-1]
 
 print("You have completed your shopping list")
 
