31df827d808e662d9de22097a75599b8543ec069
David Colson <david@peripherallabs.com>
2016-11-16 16:48:37 +0000

    Tweaks for Jupyter

--- Intro3_RockPaperScissors/RockPaperScissors.py
+++ Intro3_RockPaperScissors/RockPaperScissors.py
@@ -15,32 +15,33 @@ random.seed(time.time())
 
 command = ''
 while command != "stop":
-	command = raw_input("Please enter rock, paper or scissors: ")
-	choice = -1
-	if command == "rock":
-		choice = 0
-	elif command == "paper":
-		choice = 1
-	elif command == "scissors":
-		choice = 2
-	elif command == "stop":
-		break
+    command = input("Please enter rock, paper or scissors: ")
+    if command == "done":
+        break
+    choice = -1
+    if command == "rock":
+        choice = 0
+    elif command == "paper":
+        choice = 1
+    elif command == "scissors":
+        choice = 2
+    elif command == "stop":
+        break
 
-	computercommand = ""
-	computerchoice = int(random.randint(0, 2))
-	if computerchoice == 0:
-		computercommand = "rock"
-	elif computerchoice == 1:
-		computercommand = "paper"
-	elif computerchoice == 2:
-		computercommand = "scissors"
+    computercommand = ""
+    computerchoice = int(random.randint(0, 2))
+    if computerchoice == 0:
+    computercommand = "rock"
+    elif computerchoice == 1:
+        computercommand = "paper"
+    elif computerchoice == 2:
+        computercommand = "scissors"
 
-	print("PC chose {}".format(computercommand))
+    print("PC chose {}".format(computercommand))
 
-	if (((choice-1) % 3) == computerchoice):
-		print("You win!")
-	elif choice == computerchoice:
-		print("Tied!")
-	else:
-		print("You lose!")
-
+    if (((choice-1) % 3) == computerchoice):
+        print("You win!")
+    elif choice == computerchoice:
+        print("Tied!")
+    else:
+        print("You lose!")
